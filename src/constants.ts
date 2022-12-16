f85055cb9cf0e7ee3b07de21503af1e4e94a7541
Desmond Ademiluyi <dr.desmond@Desmonds-MacBook-Pro.local>
2022-12-16 11:02:47 -0800

    more

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,3 @@
-export const LOADING_GIF_SRC = `https://cdn.shopify.com/s/files/1/2386/2119/files/bg-clear-loader.gif?v=1652807126`;
+export const 
+LOADING_GIF_SRC = 
+`https://cdn.shopify.com/s/files/1/2386/2119/files/bg-clear-loader.gif?v=1652807126`;
