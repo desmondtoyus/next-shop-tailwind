01e207a0323398b366f56e3182012a05b3246a94
Desmond Ademiluyi <dr.desmond@Desmonds-MacBook-Pro.local>
2022-12-16 10:53:06 -0800

    more

--- src/components/common/Title/Title.tsx
+++ src/components/common/Title/Title.tsx
@@ -2,7 +2,8 @@ import React, { FC, ReactNode } from 'react';
 
 const Title: FC<{ children: ReactNode }> = ({ children }) => {
   return (<h1 className="text-2xl pb-4">{
-    children}</h1>);
+    children}
+    </h1>);
 };
 
 export default React.memo(Title);
