9be1afdaeaa7287577ee46a7b42a78434f002770
Desmond <desmondtoyus@gmail.com>
2022-12-22 12:11:51 -0800

    updates

--- src/components/ui/Buttons/Button.tsx
+++ src/components/ui/Buttons/Button.tsx
@@ -5,15 +5,18 @@ export interface ButtonProps {
   cta: string;
   onClick?: (e?: MouseEvent) => void;
   href?: string | null;
+  type?: 'button' | 'submit' | 'reset' | undefined;
 }
 
 const Button: FC<ButtonProps> = ({
   cta,
   onClick = () => null,
   href = null,
+  type = 'button',
 }) => {
   const Btn = () => (
     <button
+      type={type}
       className="p-2 rounded-md bg-blue-300 text-center text-zinc-50 w-full"
       onClick={() => onClick()}
     >
